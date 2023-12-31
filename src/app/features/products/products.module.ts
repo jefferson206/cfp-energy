8289c4b4eb99144463cbdcc58120a83327945d31
Jefferson Mendonça <43391113+jefferson206@users.noreply.github.com>
2023-12-31 00:06:55 -0300

    Removed imports non used products.module.ts

--- src/app/features/products/products.module.ts
+++ src/app/features/products/products.module.ts
@@ -1,8 +1,6 @@
 import { NgModule } from '@angular/core';
 import { OurProductsComponent } from './our-products/our-products.component';
 import { BestProductsComponent } from './best-products/best-products.component';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
 import { ProductsRouting } from './products-routing.module';
 import { CardItemModule } from 'src/libs/components/card-item/card-item.module';
 import { MatSelectModule } from '@angular/material/select';
@@ -14,11 +12,8 @@ import { SharedLibsModule } from 'src/libs/utils/shared-libs.module';
 import { ProductDetailComponent } from './product-detail/product-detail.component';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatDialogModule } from '@angular/material/dialog';
 import { CrudProductsComponent } from './crud-products/crud-products.component';
 import { NgxMaskModule } from 'ngx-mask';
 
@@ -43,9 +38,6 @@ import { NgxMaskModule } from 'ngx-mask';
     MatIconModule,
     MatButtonModule,
     NgxMaskModule.forRoot()
-    // MatToolbarModule,
-    // MatCheckboxModule,
-    // MatDialogModule,
   ],
   exports: [
     OurProductsComponent,
